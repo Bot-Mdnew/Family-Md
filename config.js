3c2da46c0d29542decfcf83d28196168a4f5eebd
Bot-Mdnew <107977961+Bot-Mdnew@users.noreply.github.com>
2022-06-26 16:30:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 812-5154-9899'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
